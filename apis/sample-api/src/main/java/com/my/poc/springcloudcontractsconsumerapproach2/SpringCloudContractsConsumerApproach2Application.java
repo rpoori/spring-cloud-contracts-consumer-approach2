a55bd4f8883c11ed1623748203f0b798495a7025
Poornima Rajendhran <poornimarajendhran@Poornimas-Air.home>
2020-07-07 07:36:19 -0400

    Update app name

---
+++ apis/sample-api/src/main/java/com/my/poc/springcloudcontractsconsumerapproach2/SpringCloudContractsConsumerApproach2Application.java
@@ -0,0 +1,13 @@
+package com.my.poc.springcloudcontractsconsumerapproach2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractsConsumerApproach2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractsConsumerApproach2Application.class, args);
+	}
+
+}
